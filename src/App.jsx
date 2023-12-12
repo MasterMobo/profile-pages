c11caa3667a6cdbd89284d09c6e9adee0bd0cf8b
MasterMobo <mastermobo101@gmail.com>
2023-12-12 16:44:19 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+// import "./App.css";
+import TutorProfilePage from "./TutorProfilePage";
+import StudentProfilePage from "./StudentProfilePage";
+
+function App() {
+    return <StudentProfilePage />;
+}
+
+export default App;
